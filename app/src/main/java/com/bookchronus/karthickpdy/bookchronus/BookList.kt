d908a42ef52cdad41b530499fc5f73d0cbf973e4
Srinivasan <karthickpdy@gmail.com>
2017-07-05 09:37:02 +0530

    Initial commit

---
+++ app/src/main/java/com/bookchronus/karthickpdy/bookchronus/BookList.kt
@@ -0,0 +1,12 @@
+package com.bookchronus.karthickpdy.bookchronus
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class BookList : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_book_list)
+    }
+}
