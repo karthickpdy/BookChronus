7f515693c9507e86f52365797d7439a1de336752
Jyoti Dubey <jyoti@freshdesk.com>
2017-07-08 13:03:27 +0530

    First commit

--- app/src/main/java/com/bookchronus/karthickpdy/bookchronus/ui/book/activity/BookListActivity.kt
+++ app/src/main/java/com/bookchronus/karthickpdy/bookchronus/ui/book/activity/BookListActivity.kt
@@ -1,9 +1,10 @@
-package com.bookchronus.karthickpdy.bookchronus
+package com.bookchronus.karthickpdy.bookchronus.ui.book.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.bookchronus.karthickpdy.bookchronus.R
 
-class BookList : AppCompatActivity() {
+class BookListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
